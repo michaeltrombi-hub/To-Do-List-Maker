a77914b0f4067e202b423c3dad14a02ac6d7736a
michaeltrombi-hub <56103076+michaeltrombi-hub@users.noreply.github.com>
2019-12-22 13:38:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function onDeviceReady() {
     admob.setOptions({
         publisherId:          "pub-8279078074288110",                    // Required
         interstitialAdId:     "ca-app-pub-8279078074288110/1995118464",  // Optional
-        tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
-        tappxIdAndroid:       "",        // Optional
-        tappxShare:           0.5                                        // Optional
     });
 }
 
